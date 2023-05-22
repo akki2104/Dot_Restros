9efb53173aa6d816ceb320ba6432dea285e4ce1e
Akash Yadav <89795799+akki2104@users.noreply.github.com>
2023-05-22 16:19:01 +0530

    create header  and basic structure

---
+++ src/components/Flyer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Flyer = () => {
+  return (
+    <>
+        <div className='flyer_main' style={ {backgroundColor:'#282828' ,width:'auto',height:"500px" } } >
+
+        </div>
+    </>
+    
+  )
+}
+
+export default Flyer
